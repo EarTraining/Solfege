c79d0e8fb2c079f2f71c00009475fe0913b6a595
EarTraining <68250652+EarTraining@users.noreply.github.com>
2020-07-13 17:55:36 -0400

    Updates

---
+++ precache-manifest.b9494174cb14ce28cc50fdd803d0ad88.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae72fd316a3296440816f5039001e1b2",
+    "url": "/Solfege/index.html"
+  },
+  {
+    "revision": "6b6c6d88f7f00cea6adb",
+    "url": "/Solfege/static/css/main.876c82b3.chunk.css"
+  },
+  {
+    "revision": "a4c7f5527bda91f079b8",
+    "url": "/Solfege/static/js/2.d74100dc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Solfege/static/js/2.d74100dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba7ce2aa75153c3f991d",
+    "url": "/Solfege/static/js/3.d9875e32.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/Solfege/static/js/3.d9875e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b6c6d88f7f00cea6adb",
+    "url": "/Solfege/static/js/main.35ebe9d1.chunk.js"
+  },
+  {
+    "revision": "862b0103202968f2d857",
+    "url": "/Solfege/static/js/runtime-main.d29a4625.js"
+  }
+]);
--- src/CounterCorrectComponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function CounterCorrectComponent ({counterCorrectProp, counterTotalProp}) {
-    return (
-        <div>
-            Score: {counterCorrectProp} / {counterTotalProp}
-        </div>
-    );
-}
